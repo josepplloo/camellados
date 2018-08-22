c592f062c0b22b07b1b58561c48296fc8214ba9e
jgarcia <jgarcia@summan.com>
2018-08-22 09:51:55 -0500

    Fixing my branch

--- camello-File/src/main/java/com/LaRutaCSV2Class.java
+++ camello-File/src/main/java/com/LaRutaCSV2Class.java
@@ -4,6 +4,8 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 public class LaRutaCSV2Class extends RouteBuilder{
+	
+	BindyCsvDataFormat mybindy = new BindyCsvDataFormat(FileCSV.class);
 
 	@Override
 	public void configure() throws Exception {
